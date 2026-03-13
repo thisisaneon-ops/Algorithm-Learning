d8ada6c3372d29e388b477f4c95de4974238a301
thisisaneon-ops <thisisaneon@gmail.com>
2026-03-13 23:59:20 +0800

    Refactor element removal functions and main demo
    Refactor removeTarget_LR and removeTarget_FS functions for better readability and efficiency. Update main function to demonstrate both methods.
    Notice: 
    be aware that the fast pointer element undergoes stable deletion while the left and right pointer elements undergo stable deletion.

---
+++ Array/Remove specified element.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+ * 删除数组中所有等于 target 的元素，并返回新长度（允许改变元素顺序）
+ * 左右指针法实现
+ */
+int removeTarget_LR(vector<int>& nums, int target)
+{
+    int i = 0;
+    int j = nums.size() - 1;
+
+    while (i <= j)
+    {
+        while (i <= j && nums[i] != target) i++;
+        while (i <= j && nums[j] == target) j--;
+        if (i < j)
+        {
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
+    }
+
+    return j + 1; // 返回新长度
+}
+
+/*
+ * 删除数组中所有等于 target 的元素，并返回新长度（保持原顺序）
+ * 快慢指针法实现
+ */
+int removeTarget_FS(vector<int>& nums, int target)
+{
+    int slow = 0;
+    for (int fast = 0; fast < nums.size(); fast++)
+    {
+        if (nums[fast] != target)
+        {
+            nums[slow++] = nums[fast];
+        }
+    }
+    return slow; // 返回新长度
+}
+
+int main()
+{
+    vector<int> nums1 = {1, 2, 3, 3, 4, 3};
+    vector<int> nums2 = nums1; // 复制一份用于另一种方法
+
+    int target = 3;
+
+    // 左右指针法
+    int newLength1 = removeTarget_LR(nums1, target);
+    cout << "左右指针法结果: ";
+    for (int i = 0; i < newLength1; i++)
+        cout << nums1[i] << " ";
+    cout << " 新长度: " << newLength1 << endl;
+
+    // 快慢指针法
+    int newLength2 = removeTarget_FS(nums2, target);
+    cout << "快慢指针法结果: ";
+    for (int i = 0; i < newLength2; i++)
+        cout << nums2[i] << " ";
+    cout << " 新长度: " << newLength2 << endl;
+
+    return 0;
+}
